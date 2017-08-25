80f826bad4a59382167f3e74c4ca245101d7b6be
Trevor Gill <trevor.a.gill@gmail.com>
2017-08-25 11:54:07 -0700

    built out basic methods in DAO for Users and Questions

--- src/main/java/dao/QuestionDao.java
+++ src/main/java/dao/QuestionDao.java
@@ -1,4 +1,20 @@
 package dao;
 
+import models.Question;
+import models.User;
+
+import java.util.List;
+
 public interface QuestionDao {
+    //create
+    void add (Question question);
+
+    void addQuestionToUser(User user, Question question);
+
+    //get all
+    List<Question> getAll();
+    List<User> getAllUsersThatAnsweredQuestion(int userId);
+
+    //find
+    Question findById(int id);
 }
--- src/main/java/dao/Sql2oQuestionDao.java
+++ src/main/java/dao/Sql2oQuestionDao.java
@@ -1,4 +1,89 @@
 package dao;
 
-public class Sql2oQuestionDao {
+import models.Question;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sql2oQuestionDao implements QuestionDao {
+    private final Sql2o sql2o;
+    public Sql2oQuestionDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add (Question question){
+        String sql = "INSERT INTO users (prompt, choices) VALUES (:prompt, :choices)";
+        try(Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql)
+                    .addParameter("prompt", question.getPrompt())
+                    .addParameter("choices", question.getChoices())
+                    .addColumnMapping("PROMPT", "prompt")
+                    .addColumnMapping("CHOICES", "choices")
+                    .executeUpdate()
+                    .getKey();
+            question.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void addQuestionToUser(User user, Question question){
+        String sql = "INSERT INTO userquestions (userid, questionid, response) VALUES (:userId, :questionId, :response)";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("userId", user.getId())
+                    .addParameter("questionId", question.getId())
+                    .addParameter("response", "response")
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Question> getAll(){
+        try(Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM questions")
+                    .executeAndFetch(Question.class);
+        }
+    }
+
+    @Override
+    public List<User> getAllUsersThatAnsweredQuestion(int questionId){
+        ArrayList<User> users = new ArrayList<>();
+
+        String joinQuery = "SELECT userId FROM userquestions WHERE questionId = :questionId";
+
+        try (Connection con = sql2o.open()) {
+            List<Integer> allUserIds = con.createQuery(joinQuery)
+                    .addParameter("questionId", questionId)
+                    .executeAndFetch(Integer.class);
+            for (Integer userId : allUserIds){
+                String questionQuery = "SELECT * FROM users WHERE id = :userId";
+                users.add(
+                        con.createQuery(questionQuery)
+                                .addParameter("userId", userId)
+                                .executeAndFetchFirst(User.class));
+            }
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+        return users;
+    }
+
+
+    @Override
+    public Question findById(int id){
+        try(Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM questions WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Question.class);
+        }
+    }
 }
